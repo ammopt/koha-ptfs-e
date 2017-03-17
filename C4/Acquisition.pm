97f75cb1e326cc60d6c60e94cde83d62679b903e
Colin Campbell <colin.campbell@ptfs-europe.com>
2017-03-17 12:17:22 +0000

    Bug 18267 populate_order_with_prices should be exported
    Although routine is used in acqui scripts, access to it
    is by backdoor methods. Allow it to be exported in a standard way
    to allow more maintainable code.
    
    Effectively this patch merely documents that this routine
    is part of C4/Acquisition's interface and allows users to import
    it in the standard way.

--- C4/Acquisition.pm
+++ C4/Acquisition.pm
@@ -40,7 +40,7 @@ use MARC::Record;
 
 use Time::localtime;
 
-use vars qw(@ISA @EXPORT);
+use vars qw(@ISA @EXPORT @EXPORT_OK);
 
 BEGIN {
     require Exporter;
@@ -91,7 +91,9 @@ BEGIN {
         &FillWithDefaultValues
         populate_order_with_prices
     );
+        @EXPORT_OK = qw( populate_order_with_prices );
 }
+
 
 
 
