9cfefe0ae279147ed45bab790c4eb944e828e54c
PTFS Admin <hosting@ptfs-europe.com>
2018-08-06 15:45:09 +0100

    Add schema files for Ill related models

--- Koha/Schema/Result/Illrequestattribute.pm
+++ Koha/Schema/Result/Illrequestattribute.pm
@@ -41,6 +41,115 @@ __PACKAGE__->table("illrequestattributes");
   data_type: 'text'
   is_nullable: 0
 
+=head2 readonly
+
+  data_type: 'tinyint'
+  default_value: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "illrequest_id",
+  {
+    data_type => "bigint",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "type",
+  { data_type => "varchar", is_nullable => 0, size => 200 },
+  "value",
+  { data_type => "text", is_nullable => 0 },
+  "readonly",
+  { data_type => "tinyint", default_value => 1, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</illrequest_id>
+
+=item * L</type>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("illrequest_id", "type");
+
+=head1 RELATIONS
+
+=head2 illrequest
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Illrequest>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "illrequest",
+  "Koha::Schema::Result::Illrequest",
+  { illrequest_id => "illrequest_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2018-08-06 15:44:07
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:F9ZFCs3z+f0tdv+r1Itoaw
+# These lines were loaded from '/usr/share/koha/lib/Koha/Schema/Result/Illrequestattribute.pm' found in @INC.
+# They are now part of the custom portion of this file
+# for you to hand-edit.  If you do not either delete
+# this section or remove that file from @INC, this section
+# will be repeated redundantly when you re-create this
+# file again via Loader!  See skip_load_external to disable
+# this feature.
+
+use utf8;
+package Koha::Schema::Result::Illrequestattribute;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::Illrequestattribute
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<illrequestattributes>
+
+=cut
+
+__PACKAGE__->table("illrequestattributes");
+
+=head1 ACCESSORS
+
+=head2 illrequest_id
+
+  data_type: 'bigint'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 type
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 200
+
+=head2 value
+
+  data_type: 'text'
+  is_nullable: 0
+
 =cut
 
 __PACKAGE__->add_columns(
@@ -91,6 +200,11 @@ __PACKAGE__->belongs_to(
 
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2017-11-09 11:42:45
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:COXHn3Q7RglF6Cl7kKHqLA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
+# End of lines loaded from '/usr/share/koha/lib/Koha/Schema/Result/Illrequestattribute.pm'
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
--- Koha/Schema/Result/SocialData.pm
+++ Koha/Schema/Result/SocialData.pm
@@ -26,6 +26,110 @@ __PACKAGE__->table("social_data");
 =head2 isbn
 
   data_type: 'varchar'
+  is_nullable: 0
+  size: 30
+
+=head2 num_critics
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 num_critics_pro
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 num_quotations
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 num_videos
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 score_avg
+
+  data_type: 'decimal'
+  is_nullable: 1
+  size: [5,2]
+
+=head2 num_scores
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "isbn",
+  { data_type => "varchar", is_nullable => 0, size => 30 },
+  "num_critics",
+  { data_type => "integer", is_nullable => 1 },
+  "num_critics_pro",
+  { data_type => "integer", is_nullable => 1 },
+  "num_quotations",
+  { data_type => "integer", is_nullable => 1 },
+  "num_videos",
+  { data_type => "integer", is_nullable => 1 },
+  "score_avg",
+  { data_type => "decimal", is_nullable => 1, size => [5, 2] },
+  "num_scores",
+  { data_type => "integer", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</isbn>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("isbn");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2018-08-06 15:44:07
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:tTNCHWxfxE3HZK657MZURg
+# These lines were loaded from '/usr/share/koha/lib/Koha/Schema/Result/SocialData.pm' found in @INC.
+# They are now part of the custom portion of this file
+# for you to hand-edit.  If you do not either delete
+# this section or remove that file from @INC, this section
+# will be repeated redundantly when you re-create this
+# file again via Loader!  See skip_load_external to disable
+# this feature.
+
+use utf8;
+package Koha::Schema::Result::SocialData;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::SocialData
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<social_data>
+
+=cut
+
+__PACKAGE__->table("social_data");
+
+=head1 ACCESSORS
+
+=head2 isbn
+
+  data_type: 'varchar'
   default_value: (empty string)
   is_nullable: 0
   size: 30
@@ -95,6 +199,11 @@ __PACKAGE__->set_primary_key("isbn");
 
 # Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-10-14 20:56:21
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Medc/ZzPVp5Eq+adFoqQoA
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
+# End of lines loaded from '/usr/share/koha/lib/Koha/Schema/Result/SocialData.pm'
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
