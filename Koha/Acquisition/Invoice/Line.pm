2fd07a6f9870d318d710176444c02ad771e92e19
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2018-09-21 13:46:48 +0100

    Bug 19850: Add triggers to update orders with invoice_lines
    (cherry picked from commit 5917cdb189a5ea5760cfeeabcd4efaf8ae36c7b2)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Acquisition/Invoice/Line.pm
+++ Koha/Acquisition/Invoice/Line.pm
@@ -31,7 +31,127 @@ Koha::Acquisition::Invoice::Line - Koha Invoice Line class
 
 =head2 Class Methods
 
+=head3 order
+
+my $order = $invoice_line->order
+
+Return the order associated with this invoice line
+
 =cut
+
+sub order {
+    my ($self) = @_;
+    return Koha::Acquisition::Order->_new_from_dbic(
+        $self->_result->aqorders_ordernumber );
+}
+
+=head3 store
+
+Order line specific store method to ensure orders are updated
+
+=cut
+
+sub store {
+    my ($self) = @_;
+
+    $self->_result->result_source->schema->txn_do(
+        sub {
+
+            if ( $self->aqorders_ordernumber ) {
+                my $order = $self->order;
+
+                # Check order present
+                unless ( $order->in_storage ) {
+                    Koha::Exceptions::Object::FKConstraint->throw(
+                        broken_fk => 'ordernumber',
+                        value     => $self->aqorders_ordernumber,
+                    );
+                }
+
+                # Store invoice line
+                $self = $self->SUPER::store;
+
+                # Update order line
+                if ( $self->pre_tax_price || $self->total_price ) {
+                    my $total_pre_tax =
+                      $order->_result->aqinvoice_lines->get_column(
+                        'pre_tax_price')->sum_rs->as_query;
+                    my $total_post_tax =
+                      $order->_result->aqinvoice_lines->get_column(
+                        'total_price')->sum_rs->as_query;
+                    $order->_result->update(
+                        {
+                            tax_rate_on_receiving  => undef,
+                            unitprice_tax_excluded => $total_pre_tax,
+                            unitprice_tax_included => $total_post_tax
+                        }
+                    );
+                }
+
+            }
+            else {
+                $self = $self->SUPER::store;
+            }
+        }
+    );
+
+    return $self;
+}
+
+=head3 delete
+
+$invoice_line->delete
+
+Delete invoice with trigger to update order line
+
+=cut
+
+sub delete {
+    my ($self) = @_;
+
+    my $deleted;
+    $self->_result->result_source->schema->txn_do(
+        sub {
+            if ( $self->aqorders_ordernumber ) {
+                my $order = $self->order;
+
+                # Check order present
+                unless ( $order->in_storage ) {
+                    Koha::Exceptions::Object::FKConstraint->throw(
+                        broken_fk => 'ordernumber',
+                        value     => $self->aqorders_ordernumber,
+                    );
+                }
+
+                # Delete the invoice line
+                $deleted = $self->SUPER::delete;
+
+                # Update the order line
+                if ( $self->pre_tax_price || $self->total_price ) {
+                    my $total_pre_tax =
+                      $order->_result->aqinvoice_lines->get_column(
+                        'pre_tax_price')->sum_rs->as_query;
+                    my $total_post_tax =
+                      $order->_result->aqinvoice_lines->get_column(
+                        'total_price')->sum_rs->as_query;
+                    $order->_result->update(
+                        {
+                            tax_rate_on_receiving  => undef,
+                            unitprice_tax_excluded => $total_pre_tax,
+                            unitprice_tax_included => $total_post_tax
+                        }
+                    );
+                }
+
+            }
+            else {
+                $deleted = $self->SUPER::delete;
+            }
+        }
+    );
+
+    return $deleted;
+}
 
 =head3 type
 
