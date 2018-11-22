9a6735faab873f3be72813583a49e79929f9e046
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2018-11-22 11:40:10 +0000

    Bug 19850: Add 'GET acquisitions/funds'
    Returns an array of all funds
    
    (cherry picked from commit 2c394bffddcd8b47a17f509879ac478deef56e43)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ Koha/REST/V1/Acquisitions/Funds.pm
@@ -0,0 +1,61 @@
+package Koha::REST::V1::Acquisitions::Funds;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Koha::Acquisition::Funds;
+
+=head1 API
+
+=head2 Class Methods
+
+=head3 list
+
+    Controller function that handles listing Koha::Acquisition::Funds objects
+
+=cut
+
+sub list {
+    my $c = shift->openapi->valid_input or return;
+
+    my @funds = Koha::Acquisition::Funds->search;
+    @funds = map { _to_api($_) } @funds;
+
+    return $c->render( status => 200, openapi => \@funds);
+}
+
+=head3 _to_api
+
+    Helper function to ensure the API returns property names that refer
+    to "fund" rather than "budget"
+
+=cut
+
+sub _to_api {
+    my $fund = shift;
+
+    my $fund_hash = $fund->unblessed;
+
+    my %out;
+    @out{ map { s/budget/fund/; $_ } keys %{$fund_hash} } = values %{$fund_hash};
+
+    return \%out;
+}
+
+1;
